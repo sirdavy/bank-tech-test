90b24912523398dec3e54e3bb63e5027d5b1c780
David Scott <sirdavidscott@hotmail.com>
2023-02-21 11:07:00 +0000

    first integration test passed

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,6 +4,11 @@ class BankAccount
     @transactions = []
   end
 
+  def deposit(amount)
+    transaction = Transaction.new(Time.now.strftime("%d/%m/%Y"), 0, amount)
+    @transactions << transaction
+  end
+
   def get_transactions_with_balance
     return @transactions
   end
--- spec/integration_test_spec.rb
+++ spec/integration_test_spec.rb
@@ -8,3 +8,34 @@ RSpec.describe BankAccount do
     expect(my_account.get_transactions_with_balance.length).to eq(1)
   end
 end
+
+
+
+
+# my_account = BankAccount.new
+# my_account.deposit(1000.00)
+# expect(my_account.get_transactions.length).to eq(1)
+
+# my_account = BankAccount.new
+# my_account.deposit(1000.00)
+# my_account.withdraw(1000.00)
+# expect(my_account.get_transactions.length).to eq(2)
+
+# my_account = BankAccount.new
+# my_account.deposit(300)
+# result = my_account.gets_transactions_with_balance
+# expect(result[0][:balance])to eq (300.00)
+
+# my_account = BankAccount.new
+# my_account.deposit(400.00)
+# result = my_account.get_transactions
+# expect(result[0].credit).to eq (400.00)
+
+# my_account = BankAccount.new
+# my_account.withdraw(700.00)
+# result = my_account.get_transactions
+# expect(result[0].credit).to eq (700.00)
+
+# my_account = BankAccount.new
+# my_account.deposit(400.00)
+# expect(my_account.print_statement).to include("400.00") #not sure if this will work
